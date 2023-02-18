db2c3d43e2be6e25fa94eae09e89abab1f5cb5a1
Siounath <82611145+Esiounath@users.noreply.github.com>
2023-02-18 17:28:56 +0100

    New update

--- Database.java
+++ Database.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 class Database {
     private String password;
@@ -20,13 +18,47 @@ class Database {
 
     public Connection connexion_database() {
         try {
+            /*
+             * Téléchargez le fichier mysql-connector8.0.32.jar sur
+             * https://dev.mysql.com/downloads/connector/j/?os=26
+             * (Pour Linux)
+             * Crée un dossier lib du projet Java courant !
+             * 1.export CLASSPATH=$CLASSPATH:/chemin/vers/mysql-connector-j-8.0.32.jar
+             * ou
+             * Inconvénients : Chaque execution !
+             * 2. java -cp ~/Documents/GitHub/Javastock/lib/mysql-connector-j-8.0.32.jar
+             * Database.java
+             * 4.javac Database.java 'Compilation du code Java'
+             * 5.java Database 'Execution de la class Database'
+             */
             // étape 1: charger la classe de driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             // étape 2: créer l'objet de connexion
             Connection connexion = DriverManager.getConnection(
-                    "jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database, this.username, this.password);
-            System.out.println(connexion);
+                    "jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database
+                            + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                    this.username, this.password);
+            System.out.println("Connexion établie !");
+            // créer l'objet statement
+            Statement statement = connexion.createStatement();
 
+            // exécuter la requête SELECT
+            ResultSet resultat = statement.executeQuery("SELECT * FROM inscription");
+
+            // parcourir les résultats
+            while (resultat.next()) {
+                // Remplacer le string par la colonne de la table qui doit être au format
+                // String(Varchar)
+                String[] champ1 = { resultat.getString("day"), resultat.getString("IP") };
+                for (int i = 0; i < champ1.length; i++) {
+                    System.out.println(champ1[i]);
+                }
+
+            }
+            // fermer les objets de connexion
+            resultat.close();
+            statement.close();
+            connexion.close();
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e);
         }
